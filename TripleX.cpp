7a5f45dd0ed5e724e6c7dcccb2c3ec0c1ff0e1f4
DannyPadron09 <danny.padron09@gmail.com>
2021-04-09 15:04:10 -0400

    added in more game instructions

--- TripleX.cpp
+++ TripleX.cpp
@@ -12,22 +12,23 @@ int main()
 
     std::cout << std::endl; // New Line Code
 
-    int firstSolutionNumber = 4; // Initializing variables
-    int secondSolutionNumber = 5;
-    int thirdSolutionNumber = 6;
+    const int firstSolutionNumber = 4; // Initializing variables
+    const int secondSolutionNumber = 5;
+    const int thirdSolutionNumber = 6;
 
-    int sumOfNumbers = firstSolutionNumber + secondSolutionNumber + thirdSolutionNumber;
+    // Sum of all 3 variables
+    const int sumOfNumbers = firstSolutionNumber + secondSolutionNumber + thirdSolutionNumber;
 
-    int productOfNumbers = firstSolutionNumber * secondSolutionNumber * thirdSolutionNumber;
+    const int productOfNumbers = firstSolutionNumber * secondSolutionNumber * thirdSolutionNumber;
 
     std::cout << firstSolutionNumber << std::endl;
     std::cout << secondSolutionNumber << std::endl;
     std::cout << thirdSolutionNumber << std:: endl;
 
-    // Sum of all 3 variables
-    std::cout << sumOfNumbers << std::endl;
+    std::cout << "There are 3 numbers in the safe code" << std::endl;
+    std::cout << "The codes add up to : " << sumOfNumbers << std::endl;
 
-    std::cout << productOfNumbers;
+    std::cout << "The codes multiply to : " << productOfNumbers << std::endl;
 
     return 0;
 
