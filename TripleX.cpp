55e72e2a68243b4f787712c82a3bb2ec3b48c744
DannyPadron09 <danny.padron09@gmail.com>
2021-04-09 13:55:40 -0400

    notes added and variable initialized and working correctly

--- TripleX.cpp
+++ TripleX.cpp
@@ -3,9 +3,19 @@
 int main()
 {
 
+    // std = namespace, cout = is defined in std 
     std::cout << "Crack the safe code and get the riches!";
-    std::cout << std::endl;
+
+    std::cout << std::endl; // New Line Code
+
     std::cout << "Enter the right code or get locked out!";
+
+    std::cout << std::endl; // New Line Code
+
+    int a = 4; // Initializing variable
+
+    std::cout << a;
+
     return 0;
 
 }
