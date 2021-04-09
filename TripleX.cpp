973e38864d33994be2630810259e47001f945863
DannyPadron09 <danny.padron09@gmail.com>
2021-04-09 13:49:47 -0400

    base and intro printing out

---
+++ TripleX.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Crack the safe code and get the riches!";
+    std::cout << std::endl;
+    std::cout << "Enter the right code or get locked out!";
+    return 0;
+
+}
